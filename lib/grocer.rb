dfcdd59a1c63a254a228836a2976229936036fd5
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-21 20:22:41 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -14,13 +14,5 @@ def consolidate_cart(cart)
 
 end
 
-grocery_shelf = [
-  { :item => "CANNED BEANS", :price => 3.00, :clearance => true },
-  { :item => "CANNED CORN", :price => 2.50, :clearance => false },
-  { :item => "SALSA", :price => 1.50, :clearance => false },
-  { :item => "TORTILLAS", :price => 2.00, :clearance => false },
-  { :item => "HOT SAUCE", :price => 1.75, :clearance => false }
-]
 
-find_item_by_name_in_collection("CANNED BEANS", grocery_shelf)
   
