20fc12b2a3f2e3542688bacdac3aa433e319c6fc
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-21 20:14:36 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,6 +1,6 @@
 def find_item_by_name_in_collection(name, collection)
   collection.each_index do |item_index|
-    collection[item_index][:item] == name
+    collection[item_index][:item] 
   end
 end
 
