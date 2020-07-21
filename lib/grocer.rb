8312ec0a322b0a457026c12e822907b8252c0f83
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-21 20:58:12 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -13,16 +13,20 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   new_cart = []
-  cart.each_index do |item_old_index|
+  cart.each do |item_hash|
     # if the given item is not currently in the new cart, shovel its hash into the new cart
-    # and add a count key with a value of 1 to the entry, which is the last element in the array since we just added it
-    if !find_item_by_name_in_collection(cart[item_index][:item], new_cart)
-      new_cart << find_item_by_name_in_collection(cart[item_index][:item])
+    # and add a key:value pair of count: 1 to the thing most recently shoveled
+    if !find_item_by_name_in_collection(item_hash[:item], new_cart)
+      new_cart << item_hash
       new_cart[-1][:count] = 1
     end
-    # if the given item IS currently in the new cart, we need to figure out its index...
+    # if the given item IS currently in the new cart, we need to find it and update its count value
     else 
-      new_cart.each_index do |
+      new_cart.each do |new_cart_item_hash|
+        if item_hash[:name] == new_cart_item_hash[:name]
+          item_hash[:count] += 1
+        end
+      end
     end
   end
 end
