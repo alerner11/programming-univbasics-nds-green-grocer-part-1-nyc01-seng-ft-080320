da5e515212cf79e56dcd676fba5e594d64222f81
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-21 20:38:16 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,6 +1,8 @@
 def find_item_by_name_in_collection(name, collection)
   collection.each_index do |item_index|
     if collection[item_index].fetch(:item) == name
+      return collection[item_index]
+    end
   end
 end
 
